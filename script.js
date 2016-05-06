66d77d8f2c70df19fc78aea89ac1e02acf22b7dd
Cam <camlatimer@gmail.com>
2016-05-06 17:39:33 -0400

    swaps out resume link

--- script.js
+++ script.js
@@ -24,7 +24,7 @@
     showEls('.con-list');
   });
   $('.resu-header').click(function(){
-    window.open("https://drive.google.com/open?id=0B00DPsAoUxClV2YwYTR6Tm0yMGs");
+    window.open("https://drive.google.com/open?id=0B00DPsAoUxCldnZERjFJZHllbEU");
   })
 // end iife
 })();
